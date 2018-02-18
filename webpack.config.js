8061bab34da4dd94489ad7bf5e9a0e15ff3ad580
DK Campbell <dkcampbell@dk-campbells-macbook-pro.local>
2018-02-18 00:30:18 -0500

    try to get a build tool working.

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'app-client.js'),
+  output: {
+    path: path.join(__dirname, 'src', 'static', 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /src\//,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(true),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      mangle: true,
+      sourcemap: false,
+      beautify: false,
+      dead_code: true
+    })
+  ]
+};
